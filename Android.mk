7adc72b827051878f25bfffad31839d1666ba929
Manjunath <manjunathx.b.s@intel.com>
2012-02-13 12:40:16 +0530

    Support of ALAC decoder: play Apple lossless audio content
    BZ: 18214
    
    Patch 1 of 3
    ALAC DECODER
    The ALAC FORMAT EXTRACTOR Patch must also be installed
    in order for the decoder to build and run.
    
    For compatibility with iTunes-generated user audio
    playlists and for compatilibiy with high quality
    audio use cases the phone should be able
    play back Apple lossless audio ("ALAC") content
    generated by iTunes and other ALAC-enabled audio clients.
    
    This portion of ALAC patch set implements
    the ALAC decoder engine
    as a stagefright plug-in component, enabling
    the standard ICS media player ("AwesomePlayer") to
    decode the ALAC elementary stream generated by
    the ALAC format extractor front end.
    
    Notes:  1) first decoder revision is based on untuned
    C-reference code. Performance is adequate but
    performance analysis and optimization are ongoing, and
    future releases will include Atom-optmizations.
    2)ALAC supports output bit resolutions greater than
    and equal to R3 native audio (20/24/32); these outputs
    are truncated to 16 bits per sample in the current
    revision; 3) ALAC supports multi-channel content;
    current rev of the decoder supports mono and
    stereo only; 4) Current revision of the format
    extractor supports .m4a file containers generated
    by v8.x, v9.x, and v10.x Apple iTunes ALAC encoders.
    
    Change-Id: If63561341e5daad508b0d94161e1c21dc71807a3
    Signed-off-by: Manjunath <manjunathx.b.s@intel.com>
    Reviewed-on: http://android.intel.com:8080/35093
    Reviewed-by: Krishna, Samaga <samaga.krishna@intel.com>
    Reviewed-by: Kandasamy, Muthukumar <muthukumar.kandasamy@intel.com>
    Reviewed-by: Sikkandar D, Madar <madar.sikkandar.d@intel.com>
    Reviewed-by: M, Arulselvan <arulselvan.m@intel.com>
    Tested-by: M, Arulselvan <arulselvan.m@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

---
+++ Android.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := -O3 -msse3 -mfpmath=sse
+
+LOCAL_SRC_FILES := \
+        SoftALAC.cpp \
+        ALACBitUtilities.c \
+        ALACEngine.cpp \
+        dp_dec.c \
+        EndianPortable.c \
+        matrix_dec.c \
+        ag_dec.c
+
+LOCAL_C_INCLUDES := \
+        frameworks/base/media/libstagefright/include \
+        frameworks/base/include/media/stagefright/openmax \
+        frameworks/base/media/libstagefright/omx \
+        $(LOCAL_PATH) 
+
+LOCAL_SHARED_LIBRARIES := \
+        libstagefright libstagefright_omx libstagefright_foundation libutils
+
+LOCAL_MODULE := libstagefright_soft_alacdec
+LOCAL_MODULE_TAGS := eng
+
+include $(BUILD_SHARED_LIBRARY)
